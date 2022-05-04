a34113d05bc4ce081067ea1e3f336d16bbc1a2fa
harshada <harshadasawant22@gmail.com>
2022-05-04 11:22:03 +0530

    Arrow Function with and without data type

---
+++ Arrow.js
@@ -0,0 +1,18 @@
+var hello = function () {
+    return "Hello World!";
+};
+console.log(hello());
+var hello1 = function () {
+    return "Hello World";
+};
+console.log(hello1());
+var getResult = function (username, points) {
+    return username + ' scored ' + points + ' points!';
+};
+console.log(getResult("harshada", 100.5));
+var getArr = function (u, p) { return u + ' scored ' + p + ' points!'; };
+console.log(getArr("harshada", 100));
+var getResult1 = function (username, points) {
+    return username + ' scored ' + points + ' points!';
+};
+console.log(getResult1("harshada", 100.5));
---
+++ Arrow.ts
@@ -0,0 +1,31 @@
+// normal function 
+var hello = function() {
+    return "Hello World!";
+  }
+
+  console.log(hello());
+// Arraow Function
+var hello1=() => {
+    return "Hello World";
+}
+console.log(hello1());
+
+// is with parameter without  data type
+  var getResult = function(username, points) {  
+    return username + ' scored ' + points + ' points!';  
+  };  
+  console.log(getResult("harshada",100.5));
+
+var getArr= (u,p)=>{ return u + ' scored ' + p + ' points!'}; 
+console.log(getArr("harshada",100));
+
+//with parameter with data type
+
+  var getResult1 = function(username:string, points:number) {  
+    return username + ' scored ' + points + ' points!';  
+  };  
+  console.log(getResult1("harshada",100.5));
+//   create arrow function
+
+ 
+
