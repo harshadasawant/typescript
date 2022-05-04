5bbae0d8b217eb9cf6b42fa9442e4437d6e8baa6
harshada <harshadasawant22@gmail.com>
2022-05-05 01:55:29 +0530

    Object Destructuring

---
+++ ObjectDestructuring.js
@@ -0,0 +1,39 @@
+//prior Es6
+var user = {
+    'name': 'Alex',
+    'address': '15th Park Avenue',
+    'age': 43
+};
+var name1 = user.name;
+var age1 = user.age;
+console.log(name1, age1);
+// ES6 onwards
+var user1 = {
+    'name2': 'Alex',
+    'address2': '15th Park Avenue',
+    'age': 43
+};
+var permanentAddress = user1.address2;
+var address2 = user1.address2;
+console.log(permanentAddress); // 15th Park Avenue
+console.log(address2); // 15th Park Avenue
+var user2 = {
+    'name': 'Alex',
+    'address': '15th Park Avenue',
+    'age': 43,
+    'department': {
+        'name': 'Sales',
+        'Shift': 'Morning',
+        'address': {
+            'city': 'Bangalore',
+            'street': '7th Residency Rd',
+            'zip': 560001
+        }
+    }
+};
+var department = user2.department;
+console.log(department);
+var address = user2.department.address;
+console.log(address);
+var city = user2.department.address.city;
+console.log(city);
